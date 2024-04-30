b4ebf71e17dbfcf03cdf17ceb01363392fbd9c59
coga0057 <natasha.marie33@gmail.com>
2024-04-30 17:55:09 +0900

    finished the farm i think

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./starter/modules/replaceTemplate');
 
 //Blocking synchronous
 // const textIn = fs.readFileSync('starter/txt/input.txt', 'utf-8');
@@ -28,21 +30,37 @@ const url = require('url');
 
 ///////
 //SERVER
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/product.html`, 'utf-8');
+
 
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const {query, pathname} = (url.parse(req.url, true));
 
   //Overview page
-  if(pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+  if(pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
   //Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
-  }else if (pathName ==='/api') {
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+  //API
+  }else if (pathname ==='/api') {
       res.writeHead(200, {'Content-type': 'application/json'});
       res.end(data);
 
